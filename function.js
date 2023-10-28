b86776aadea16a30536a531d17fca43a830e98d1
Ritik Pal <er.ritikpal@gmail.com>
2023-10-29 01:01:08 +0530

    file uploaded

---
+++ function.js
@@ -0,0 +1,18 @@
+// functions
+
+function printthevalue(a,b,c){  // abc is  parameters
+    let result = (a*10 + b*10 + c*10 )/10;
+
+    
+    console.log(result);
+
+
+}
+// Function Calling 
+
+printthevalue(23,23,23); // Value that i've been passed is arguments
+printthevalue(45,45,45);
+printthevalue(45,345,45);
+printthevalue(45,456,45);
+printthevalue(45,42,45);
+printthevalue(45,74,45);
